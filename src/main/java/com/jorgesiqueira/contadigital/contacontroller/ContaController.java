771a8bb0d1a2530ab80aaea49c5224e80552825e
jorgetsiqueira <jorgetsiqueira@gmail.com>
2021-02-01 09:57:40 -0200

    Adicionando Funções
    Adicionando Funções

--- src/main/java/com/jorgesiqueira/contadigital/contacontroller/ContaController.java
+++ src/main/java/com/jorgesiqueira/contadigital/contacontroller/ContaController.java
@@ -1,15 +1,31 @@
 package com.jorgesiqueira.contadigital.contacontroller;
 
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.jorgesiqueira.contadigital.domain.model.Cliente;
+
 
 @RestController
 public class ContaController {
-	
-	@GetMapping ("/clientes")
-	public String listar () {
-		return "Teste";
+
+	@GetMapping("/clientes")
+	public List<Cliente> listar() {
+		var cliente1 = new Cliente();
+		cliente1.setId(1L);
+		cliente1.setNome("Jorge Siqueira");
+		cliente1.setCpf("999.888.777-66");
+		cliente1.setEndereco("Rua Doze de Dezembro, N164 - João Pessoa, PB.");
 		
+		var cliente2 = new Cliente();
+		cliente2.setId(2L);
+		cliente2.setNome("Miriam Targino");
+		cliente2.setCpf("999.888.777-66");
+		cliente2.setEndereco("Rua Quinze De Novembro, N57 - João Pessoa, PB.");
+
+		return Arrays.asList(cliente1, cliente2);
+
 	}
 
 }
--- src/main/java/com/jorgesiqueira/contadigital/domain/model/Cliente.java
+++ src/main/java/com/jorgesiqueira/contadigital/domain/model/Cliente.java
@@ -1,8 +1,7 @@
 package com.jorgesiqueira.contadigital.domain.model;
 
-
 //1.	Cadastro da conta:
-//a.	Campos: Nome completo, número do documento, endereço completo e senha provisória;
+//a.	Campos: Nome completo, número do documento "cpf" , endereço completo e senha provisória;
 
 public class Cliente {
 	
@@ -11,5 +10,29 @@ public class Cliente {
 	private String endereco;
 	private String cpf;
 	
-
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
 }
